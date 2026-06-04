245ae0fd3fac98b7bc267c195f5f216a1e21bced
JunYeElectronics <junye@jun-ye.com>
2026-06-04 11:30:44 +0800

    Revert JUCE 7/8 compat changes, restore JUCE 6.1.6 API
    Reverting getStringWidth/getStringWidthFloat/GlyphArrangement changes
    back to original font.getStringWidth() for JUCE 6.1.6 compatibility.
    Also reverted float types back to int in FxTheme drawDocumentWindowTitleBar.

--- fxsound/Source/GUI/FxHyperlink.cpp
+++ fxsound/Source/GUI/FxHyperlink.cpp
@@ -46,5 +46,5 @@ int FxHyperlink::getTextWidth()
 	auto& theme = dynamic_cast<FxTheme&>(LookAndFeel::getDefaultLookAndFeel());
 	auto font = theme.getNormalFont();
 	font.setUnderline(true);
-	return (int)GlyphArrangement::getStringWidth(font, getButtonText());
+	return font.getStringWidth(getButtonText());
 }
--- fxsound/Source/GUI/FxNotification.cpp
+++ fxsound/Source/GUI/FxNotification.cpp
@@ -123,7 +123,7 @@ void FxNotification::setMessage(const String& message, const std::pair<String, S
 		}
 
         auto margin = autohide ? 80 : 40;
-		line_width = (int)GlyphArrangement::getStringWidth(font, lines[i]) + link_width;
+		line_width = font.getStringWidth(lines[i]) + link_width;
 		if (line_width > WIDTH-margin)
 		{
 			if (line_width > MAX_WIDTH-margin)
@@ -164,7 +164,7 @@ void FxNotification::showMessage(bool autohide)
 		{
             auto& theme = dynamic_cast<FxTheme&>(LookAndFeel::getDefaultLookAndFeel());
 			auto font = theme.getSmallFont().withHeight(17.0f);
-			x += (int)GlyphArrangement::getStringWidth(font, message_lines_[last_line].getText());
+			x += font.getStringWidth(message_lines_[last_line].getText());
 		}
         message_link_.setBounds(x, link_line_ * 20 + 30, width, 20);
 		message_link_.setVisible(true);
